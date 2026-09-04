5a37f4138abcd2b09b273f8fabb63131159808eb
rexoxean <moon6f@gmail.com>
2026-09-04 19:32:03 +0900

    Update stamp image positioning in index.tsx.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -287,9 +287,9 @@ function Index() {
                     alt="운영본부 직인"
                     className="pointer-events-none absolute z-10 w-[78px] select-none"
                     style={{
-                      left: "75px",
-                      top: "20px",
-                      transform: "rotate(16deg)",
+                      right: "75px",
+                  bottom: "20px",
+                  transform: "rotate(16deg)",
                     }}
                   />
                 </div>
